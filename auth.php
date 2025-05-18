705dcbc93b32ac789bacd0709ac95703d1d1876d
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-05-18 14:33:33 +0300

    Implemented a function to generate an auth key on login. Modified login function to use that function for key creation.

--- auth.php
+++ auth.php
@@ -1,14 +1,23 @@
 <?php
-function login($username, $password): bool
+function login($username, $password): string
 {
 	require('db.php');
 	/** @var \PgSql\Connection $db */
 	$result = pg_query_params($db, 'SELECT user_id, username, password FROM users WHERE username=$1', array($username));
 	$row = pg_fetch_all($result)[0];
-	return (password_verify($password, $row['password']));
+	if (password_verify($password, $row['password'])) {
+		return gen_auth_key($row['user_id']);
+	} else {
+		return '';
+	}
 }
 
-function gen_auth_key()
+function gen_auth_key($user_id)
 {
-
+	require('db.php');
+	/** @var \PgSql\Connection $db */
+	$result = pg_query_params($db, 'insert into sessions (user_id) values ($1) returning session_id', array($user_id));
+	$row = pg_fetch_all($result);
+	print_r($row);
+	return $row['session_id'];
 }
