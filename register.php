9a39b02be907427ff140df278aa7d5f74bcbd1ca
DovgaNik <nikita.dovgan.05@hotmail.com>
2025-05-18 12:20:29 +0300

    Added link to github in the title.
    Implemented registration.

---
+++ register.php
@@ -0,0 +1,70 @@
+<?php
+require("title.php");
+
+require('db.php');
+/** @var \PgSql\Connection $db */
+?>
+
+    <form action="register.php" method="post">
+
+        <label>
+            Login:
+            <input type="text" name="username">
+        </label>
+        <br>
+
+        <label>
+            Password:
+            <input type="password" name="password">
+        </label>
+        <br>
+
+        <label>
+            First name:
+            <input type="text" name="first_name">
+        </label>
+        <br>
+
+        <label>
+            Last name:
+            <input type="text" name="last_name">
+        </label>
+        <br>
+
+        <input type="submit" value="Register">
+
+    </form>
+
+<?php
+if (isset($_POST['username'])) { // By checking where username var is set, we know whether the request is get or post
+	if (empty($_POST['username'])) {
+		echo("The username is empty <br>");
+		exit;
+	}
+	if (empty($_POST['password'])) {
+		echo("The password is empty <br>");
+		exit;
+	}
+	if (empty($_POST['first_name'])) {
+		echo("The first name is empty <br>");
+		exit;
+	}
+	if (empty($_POST['last_name'])) {
+		echo("The last name is empty <br>");
+		exit;
+	}
+
+	// if we reach this point, all the vars are set
+	$password_hash = password_hash($_POST['password'], PASSWORD_BCRYPT);
+
+	$result = pg_query_params($db,
+		'insert into users (username, password, first_name, last_name) values ($1, $2, $3, $4) returning user_id',
+		array(
+			$_POST['username'],
+			$password_hash,
+			$_POST['first_name'],
+			$_POST['last_name']
+		));
+    pg_query($db, 'commit');
+}
+?>
--- title.php
+++ title.php
@@ -2,3 +2,5 @@
 <h2>Project, for the class of Advanced Web Programming</h2>
 <h3>Student: Nikita Dovhan</h3>
 <h3>Scientific Coordinator: Alexandru Enaceanu</h3>
+<a href="https://github.com/DovgaNik/php_project">Link to the project's source code</a>
+<hr>
